36e0657395c54d25ba39c1f2066f16d7493421d1
IslomW <119986336+IslomW@users.noreply.github.com>
2025-02-27 19:37:21 +0900

    feat(NotificationEventHandler.java): add EventHandler

--- src/main/java/com/sharipov/notification_service/handler/NotificationEventHandler.java
+++ src/main/java/com/sharipov/notification_service/handler/NotificationEventHandler.java
@@ -1,2 +1,17 @@
-package com.sharipov.notification_service.handler;public class NotificationEventHandler {
+package com.sharipov.notification_service.handler;
+
+import com.sharipov.notification_service.event.NotificationEvent;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@KafkaListener(topics = "notification-event-topic")
+public class NotificationEventHandler {
+
+
+    @KafkaHandler
+    public void handle(NotificationEvent notificationEvent) {
+
+    }
 }
